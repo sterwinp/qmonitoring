4eaeb5c0a0b3ddb500faf5f852131a0dcae53562
sterwin <sterwinece@gmail.com>
2019-07-19 11:39:23 +0530

    server time clock added

--- application/views/home.php
+++ application/views/home.php
@@ -50,7 +50,8 @@
         </nav>
       </div>
        <div class="container header-fixed-color margin-bottom-10 ">
-        <div class="notes-top"> <i class="fa fa-home"></i> <span class="notes-txt">Dashboard</span>
+        <div class="notes-top"> <div class="pull-left"><i class="fa fa-home"></i> <span class="notes-txt">Dashboard</span></div>
+      <div class="srvr-tme  pull-right"><b class="sys-time"><span class="profile-time" id="profile-time"><?php //echo date('h:i:s a');?></span></b></div>
       </div>
     </div>
     </div>
@@ -159,5 +160,49 @@
   var base_url = '<?php echo base_url();?>';
   var query = '<?php echo json_encode( $query_details );?>';
 </script>
+<script>
+        flag_time = true;
+        timer = '';
+        setInterval(function(){phpJavascriptClock();},1000);
+        
+        function phpJavascriptClock()
+        {
+          var date  = '<?php echo date('M D d Y').' '.date('h:i:s a');?>';
+          date      = new Date(date).getTime();
+          if ( flag_time ) {
+          timer = date;
+          }
+          var d = new Date(timer);
+          months = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec');
+          
+          month_array = new Array('January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'Augest', 'September', 'October', 'November', 'December');
+          
+          day_array = new Array( 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+          
+          currentYear = d.getFullYear();
+          month = d.getMonth();
+          var currentMonth = months[month];
+          var currentMonth1 = month_array[month];
+          var currentDate = d.getDate();
+          currentDate = currentDate < 10 ? '0'+currentDate : currentDate;
+          
+          var day = d.getDay();
+          current_day = day_array[day];
+          var hours = d.getHours();
+          var minutes = d.getMinutes();
+          var seconds = d.getSeconds();
+          
+          var ampm = hours >= 12 ? 'PM' : 'AM';
+          hours = hours % 12;
+          hours = hours ? hours : 12; // the hour ’0′ should be ’12′
+          minutes = minutes < 10 ? '0'+minutes : minutes;
+          seconds = seconds < 10 ? '0'+seconds : seconds;
+          var strTime = hours + ':' + minutes+ ':' + seconds + ' ' + ampm;
+          timer = timer + 1000;
+          document.getElementById("profile-time").innerHTML= currentMonth+' ' + currentDate+'  ' + currentYear + ' ' + strTime ;
+          flag_time = false;
+        }
+
+    </script>
 </body>
 </html>
